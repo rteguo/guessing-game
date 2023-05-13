8788150e52ade44655b8efcb50fc51c753396e13
rteguo <tegsromaric@gmail.com>
2023-05-12 22:25:39 -0500

    Input nuber to check and generate the random number

--- game.py
+++ game.py
@@ -1,5 +1,8 @@
 """A number-guessing game."""
-
+import random
+from random import randint
 # Put your code here
 print("what's your name?")
-
+number_check = input("Give a number : ")
+secret_number = random.randint(1, 10)
+print(secret_number)
